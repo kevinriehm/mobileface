b90904db02c66345cdbcfd262b6b0d943aeff998
Kevin Riehm <kevin.riehm@gmail.com>
2014-06-24 00:58:29 -0400

    Added some debug logging during Twitter auth

--- src/com/kevinriehm/mobileface/TwitterAuthActivity.java
+++ src/com/kevinriehm/mobileface/TwitterAuthActivity.java
@@ -2,12 +2,17 @@ package com.kevinriehm.mobileface;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class TwitterAuthActivity extends Activity {
+	private final static String TAG = "MobileFace-TwitterAuthActivity";
+
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
@@ -15,9 +20,18 @@ public class TwitterAuthActivity extends Activity {
 
 		WebView webview = (WebView) findViewById(R.id.twitter_auth_webview);
 		webview.setWebViewClient(new WebViewClient() {
+			public void onPageStarted(WebView view, String url, Bitmap favicon) {
+				shouldOverrideUrlLoading(view,url);
+			}
+
 			public boolean shouldOverrideUrlLoading(WebView view, String url) {
+				Log.i(TAG,url);
+				Log.i(TAG,getResources().getString(R.string.twitter_callback_url));
+
 				// Is this the callback we gave to Twitter?
 				if(url.startsWith(getResources().getString(R.string.twitter_callback_url))) {
+					Log.i(TAG,"finishing");
+
 					Intent result = new Intent();
 					result.setData(Uri.parse(url));
 					setResult(RESULT_OK,result);
@@ -27,10 +41,14 @@ public class TwitterAuthActivity extends Activity {
 					return true;
 				}
 
-				// Nope; carry on
+				// Nope, carry on
 				return false;
 			}
 		});
+
+		WebSettings settings = webview.getSettings();
+		settings.setSaveFormData(false);
+		settings.setSavePassword(false);
 
 		webview.loadUrl(getIntent().getDataString());
 	}
