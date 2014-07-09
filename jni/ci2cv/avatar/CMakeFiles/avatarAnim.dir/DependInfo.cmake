75f400ab8fade0e6f744b34c42201329cf8bf7d3
Kevin Riehm <kevin.riehm@gmail.com>
2014-07-09 12:28:16 -0400

    Initial import of CI2CV expression transfer code

---
+++ jni/ci2cv/avatar/CMakeFiles/avatarAnim.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/kevin/face-analysis-sdk/src/avatar/Avatar.cpp" "/home/kevin/face-analysis-sdk/src/avatar/CMakeFiles/avatarAnim.dir/Avatar.cpp.o"
+  "/home/kevin/face-analysis-sdk/src/avatar/myAvatar.cpp" "/home/kevin/face-analysis-sdk/src/avatar/CMakeFiles/avatarAnim.dir/myAvatar.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "_FILE_OFFSET_BITS=64"
+  "_LARGEFILE_SOURCE"
+  "_WITH_AVATAR_"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/kevin/face-analysis-sdk/src/utils/CMakeFiles/utilities.dir/DependInfo.cmake"
+  "/home/kevin/face-analysis-sdk/src/tracker/CMakeFiles/clmTracker.dir/DependInfo.cmake"
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/include/opencv"
+  "src"
+  "src/avatar"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ jni/ci2cv/avatar/CMakeFiles/avatarAnim.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/avatarAnim.dir/Avatar.cpp.o"
+  "CMakeFiles/avatarAnim.dir/myAvatar.cpp.o"
+  "../../lib/libavatarAnim.pdb"
+  "../../lib/libavatarAnim.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/avatarAnim.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
